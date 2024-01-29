e8017b89355a1f46c8021177bfbeb9a7af864de2
Adrien Verhaeghe <adrienvrh@proton.me>
2024-01-29 13:51:16 +0100

    mongoose id init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://adrienverhaeghe:fBZoFS28mQT0AWj9@cluster0.00lh5ii.mongodb.net/',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
