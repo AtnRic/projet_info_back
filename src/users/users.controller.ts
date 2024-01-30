369ef698e37f94bb0a5b3591c406fc1a4a2c34f2
Adrien Verhaeghe <adrienvrh@proton.me>
2024-01-30 13:09:13 +0100

    user findall

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, UseGuards, Request } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get('findAll')
+  @UseGuards(AuthGuard)
+  async findAll(@Request() req) {
+    console.log(req);
+    const mongoId = await this.usersService.findAll();
+    return mongoId;
+  }
 }
