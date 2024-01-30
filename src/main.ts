3797dcc2584daa9d10ef1594b7038b87bb802213
Benjamin Soulas <benji.soulas@gmail.com>
2024-01-30 14:58:43 +0100

    Images static serving

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import {NestExpressApplication} from "@nestjs/platform-express";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({
     origin: '*',
     methods: '*',
+  });
+  app.useStaticAssets(join(__dirname, '..', 'images'), {
+    prefix: '/public',
+    index: false
   });
   await app.listen(3000);
 }
